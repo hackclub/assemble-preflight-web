442ec15a1ad4a44e1c554a84422a47275f444b71
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-07-19 16:58:36 -0700

    complete oauth

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -5,13 +5,18 @@ import fetch from 'node-fetch';
 export default async function (req, res) {
     const cookies = new Cookies(req, res);
     const { auth_code } = req.query;
-    const codeVerifier = cookies.get('indocs_code_verifier');
+    const codeVerifier = cookies.get('assemble_code_verifier');
     const codeChallenge = crypto
       .createHash('sha256')
       .update(codeVerifier)
       .digest('base64url');
     const url = `https://api.allotrope.id/oauth/token?auth_code=${encodeURIComponent(auth_code)}&code_verifier=${encodeURIComponent(codeVerifier)}&code_challenge=${encodeURIComponent(codeChallenge)}&method=S256`;
     const response = await fetch(url).then(response => response.json());
-    console.log(response);
-    res.json(response);
+    const oneHrInMs = 60 * 60 * 1000;
+    cookies.set('assemble_access_token', codeVerifier, {
+      overwrite: true,
+      httpOnly: true,
+      expires: new Date(Date.now() + oneHrInMs),
+    });
+    res.redirect('/vaccine');
 }
---
+++ pages/vaccine.js
@@ -0,0 +1,7 @@
+export default function Vaccine () {
+    return (
+        <div>
+            <h1>Vaccine</h1>
+        </div>
+    )
+}
