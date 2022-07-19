70a69306f54beb19f28fbb4244f0e1175a170589
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-07-19 16:49:55 -0700

    fix auth

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -2,7 +2,7 @@ import Cookies from 'cookies';
 import crypto from 'node:crypto';
 import fetch from 'node-fetch';
 
-export default function (req, res) {
+export default async function (req, res) {
     const cookies = new Cookies(req, res);
     const { auth_code } = req.query;
     const codeVerifier = cookies.get('indocs_code_verifier');
@@ -10,6 +10,8 @@ export default function (req, res) {
       .createHash('sha256')
       .update(codeVerifier)
       .digest('base64url');
-    const response = fetch(`https://api.allotrope.id/oauth/token?auth_code=${encodeURIComponent(auth_code)}&code_verifier=${encodeURIComponent(codeVerifier)}&code_challenge=${encodeURIComponent(codeChallenge)}`).then(response => response.json());
+    const url = `https://api.allotrope.id/oauth/token?auth_code=${encodeURIComponent(auth_code)}&code_verifier=${encodeURIComponent(codeVerifier)}&code_challenge=${encodeURIComponent(codeChallenge)}&method=S256`;
+    const response = await fetch(url).then(response => response.json());
+    console.log(response);
     res.json(response);
 }
