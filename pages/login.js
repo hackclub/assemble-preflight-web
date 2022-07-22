825940614383bd5e4cd1409cf3555f9691718612
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-07-21 22:50:56 -0700

    Style loading page

--- pages/login.js
+++ pages/login.js
@@ -2,37 +2,39 @@ import getAuthorizeURL from '../lib/server/auth.js'
 import { useEffect } from 'react'
 import getCookie from '../lib/cookie.js'
 
-export default function Login ({ loginUrl, error }) {
-    useEffect(() => {
-      try {
-        let cookie = getCookie('assemble_access_token');
-        if (cookie && !error) {
-          location.replace('/');
-        } else {
-          location.replace(loginUrl);
-        }
-      } catch (err) {
+export default function Login({ loginUrl, error }) {
+  useEffect(() => {
+    try {
+      let cookie = getCookie('assemble_access_token');
+      if (cookie && !error) {
+        location.replace('/');
+      } else {
         location.replace(loginUrl);
       }
-    }, []);
-    return (
-        <div>
-            <h1>Redirecting...</h1>
-            <p>
-              Not working? <a href={loginUrl} style={{ color: '#fa4639' }}>Click here</a> to access it.
-            </p>
-            {error && <p>{error}</p>}
-        </div>
-    )
+    } catch (err) {
+      location.replace(loginUrl);
+    }
+  }, []);
+  return (
+    <Box py={3} sx={{ minHeight: '100vh', backgroundImage: 'linear-gradient(90deg, rgba(2,0,36,0.37718837535014005) 0%, rgba(2,0,36,0.36318277310924374) 35%, rgba(2,0,36,0.36878501400560226) 100%), url(https://cloud-2ppyw38ar-hack-club-bot.vercel.app/0golden-bay.png)', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundSize: 'cover' }}>
+      <Heading sx={{ color: 'white' }}>
+        Redirecting...
+      </Heading>
+      <Box>
+        Not working? <a href={loginUrl} style={{ color: '#fa4639' }}>Click here</a> to access it.
+      </Box>
+      {error && <Box>{error}</Box>}
+    </Box>
+  )
 }
 
 export function getServerSideProps(ctx) {
-    const props = {
-        loginUrl: getAuthorizeURL(ctx.req, ctx.res, ctx.query),
-        error: ctx?.query?.error?.toString() || ''
-    };
-  
-    return {
-        props
-    };
-  }
+  const props = {
+    loginUrl: getAuthorizeURL(ctx.req, ctx.res, ctx.query),
+    error: ctx?.query?.error?.toString() || ''
+  };
+
+  return {
+    props
+  };
+}
--- pages/vaccine.js
+++
@@ -1,7 +0,0 @@
-export default function Vaccine () {
-    return (
-        <div>
-            <h1>Vaccine</h1>
-        </div>
-    )
-}
