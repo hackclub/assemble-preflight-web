dbf9b52040dcdefd1dc509923002adac20a72d91
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-07-22 23:25:12 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -223,7 +223,7 @@ export default function Home() {
                 opacity: 0.5,
               }}
             >
-              Required: Negative ART Test
+              Required: Negative COVID Test
             </Box>
             <Box
               bg="sunken"
@@ -365,11 +365,11 @@ export default function Home() {
                 p={3}
                 mb={3}
                 as="a"
-                href={process.env.NEXT_PUBLIC_APPSTORE_URL}
-                style={{ display: "block", bg: "sunken", borderRadius: 3 }}
+                // href={process.env.NEXT_PUBLIC_APPSTORE_URL}
+                style={{ display: "block", bg: "sunken", borderRadius: 3, opacity: 0.5 }}
               >
                 <Heading mb={2}>
-                  iOS App <span className="arrow">&rarr;</span>
+                  iOS App (coming soon) <span className="arrow">&rarr;</span>
                 </Heading>
                 <Box>
                   Download the iOS app for preflight, vaccine verification, and
