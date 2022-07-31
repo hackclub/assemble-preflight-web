6b7796be4654d1e90adc0e8cd41e5b96dd45b3dd
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-07-31 02:46:29 -0700

    verify alternate cookie

--- pages/api/get-auth-state.js
+++ pages/api/get-auth-state.js
@@ -3,6 +3,7 @@ import Cookies from 'cookies';
 export default async function (req, res) {
     const cookies = new Cookies(req, res);
     const token = cookies.get('assemble_access_token');
-    if (token) return res.send('TRUE');
+    const altToken = cookies.get('hackclub-ticketing-auth');
+    if (token && altToken) return res.send('TRUE');
     else return res.send('FALSE');
 }
