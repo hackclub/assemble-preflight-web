26f2b8df82b2ab8e69f4532f65b17434b47d649a
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-08-03 18:38:28 -0700

    stuff

---
+++ pages/api/ticket.js
@@ -0,0 +1,27 @@
+import Cookies from "cookies";
+import fetch from 'node-fetch';
+
+export function Try (fn) {
+    try {
+        return fn();
+    } catch (e) {
+        return null;
+    }
+}
+
+export default async function (req, res) {
+  const cookies = new Cookies(req, res);
+  const token = cookies.get("assemble_access_token");
+  fetch('https://api.ticketing.assemble.hackclub.com/tickets/qr', {
+    headers: {
+        Authorization: 'Bearer ' + token
+    }
+  }).then(response => response.text()).then(response => {
+    let json = Try(() => JSON.parse(response));
+    if (json) {
+        res.json(json);
+    } else {
+        res.send(response);
+    }
+  })
+}
