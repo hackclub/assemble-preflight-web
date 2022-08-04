800d8c19bc787d1e90f0c6773b7e07f6bf99461a
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-08-03 20:11:10 -0700

    hdfgkd

--- pages/api/wallet.js
+++ pages/api/wallet.js
@@ -17,14 +17,18 @@ export default async function (req, res) {
       Authorization: "Bearer " + token,
     },
   })
-    .then((response) => response.text())
     .then((response) => {
-        let json = Try(() => JSON.parse(response));
-        if (json) {
-          res.status(404).send("ERROR\n\n" + JSON.stringify(json, null, 4));
+        if (response.status == 200) {
+            res.setHeader('Content-Type', 'application/vnd.apple.pkpass');
+            const headers = Object.fromEntries(response.headers);
+            for (const header in headers) {
+                res.setHeader(header, headers[header]);
+            }
+            return res.send(await response.buffer());
         } else {
-          res.setHeader('Content-Type', 'application/vnd.apple.pkpass');
-          res.send(response);
+            response.json().then(json => {
+                res.status(404).json(json);
+            })
         }
     });
 }
